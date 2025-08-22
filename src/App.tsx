58a05a30764de97a436102042e76f6670bb75fc9
kartik23km <malhotrakartik5@gmail.com>
2025-08-23 01:28:16 +0530

    initial setup and tailwind configured

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 p-5 rounded-lg text-white w-fit mx-auto">
+        Initial setup
+      </div>
+    </>
+  );
+}
+
+export default App;
