889a877acb040146833ac16019694b3a96f1a41f
kartik23km <malhotrakartik5@gmail.com>
2025-08-23 01:33:39 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="bg-blue-500 p-5 rounded-lg text-white w-fit mx-auto">
-        Initial setup
+        Initial setups
       </div>
     </>
   );
